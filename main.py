5842e54203c8731fcbe948a4f7da0fe1a8d75179
DimaDepp <gapko03@mail.ru>
2025-10-22 11:08:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "core"))
+# main.py
 import asyncio
-import sys, os
-
-# добавляем путь, чтобы core находился всегда
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "core"))
-
-from core.telegram_bot import main as start_bot
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(start_bot())
-    except RuntimeError:
-        loop = asyncio.get_event_loop()
-        loop.create_task(start_bot())
-        loop.run_forever()
+from telegram_bot import main as start_bot  # вместо from core.telegram_bot
